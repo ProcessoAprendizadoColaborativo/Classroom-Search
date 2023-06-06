8793406a240701fbd9c2611aff40754e67c95489
ruanhoffmann <ruan.hoffmann1@gmail.com>
2023-06-06 15:24:36 -0300

    criando index.html

--- main.py
+++ main.py
@@ -2,25 +2,24 @@ import os
 import mysql.connector
 from dotenv import load_dotenv
 from flask import Flask, jsonify, request
+
 load_dotenv()
-
 app = Flask(__name__)
-
 cnx = mysql.connector.connect(
-    host= os.environ.get("DB_HOST"),
-    port= int(os.environ.get("DB_PORT")),
-    user=os.environ.get("DB_USER"),
-    database=os.environ.get("DB_NAME")
+    host=os.environ.get("DB_HOST"),
+    database=os.environ.get("DB_NAME"),
+    port=int(os.environ.get("DB_PORT")),
+    user=os.environ.get("DB_USER")
 )
 
-cursor = cnx.cursor()
+corsa = cnx.cursor()
 
 # Endpoint para listar todas as salas
 @app.route('/', methods=['GET'])
 def get_salas():
-    cursor = cnx.cursor()
-    cursor.execute("SELECT * FROM salas")
-    result = cursor.fetchall()
+  
+    corsa.execute("SELECT * FROM salas")
+    result = corsa.fetchall()
     users = []
     for user in result:
         user_dict = {
@@ -38,12 +37,43 @@ def add_sala():
     id_sala = data['id_sala']
     ambiente = data['ambiente']
     posicao = data['posicao']
-    cursor = mydb.cursor()
-    sql = "INSERT INTO users (id_sala, ambiente, posicao) VALUES (%s, %s, %s)"
+    sql = "INSERT INTO salas (id_sala, ambiente, posicao) VALUES (%s, %s, %s)"
     values = (id_sala, ambiente, posicao)
-    cursor.execute(sql, values)
-    mydb.commit()
+    corsa.execute(sql, values)
+    corsa.commit()
     return jsonify({"message": "Sala adicionada com sucesso"})
+
+
+# Endpoint para listar todas as turmas
+@app.route('/', methods=['GET'])
+def get_turmas():
+    corsa.execute("SELECT * FROM turmas")
+    result = corsa.fetchall()
+    users = []
+    for user in result:
+        user_dict = {
+            "id_turma": user[0],
+            "qtd_alunos": user[1],
+            "turno": user[2],
+            "disponibilidade": user[3]
+        }
+        users.append(user_dict)
+    return jsonify(users)
+
+
+# Endpoint para adicionar uma nova turma
+@app.route('/criar-turmas', methods=['POST'])
+def add_turma():
+    data = request.json
+    id_turma = data['id_turma']
+    qtd_alunos = data['qtd_alunos']
+    turno = data['turno']
+    diponibilidade = data["disponibilidade"]
+    sql = "INSERT INTO turmas (id_turma, qtd_alunos, turno, disponibilidade) VALUES (%s, %s, %s, %s)"
+    values = (id_turma, qtd_alunos, turno, disponibilidade)
+    corsa.execute(sql, values)
+    corsa.commit()
+    return jsonify({"message": "Turma adicionada com sucesso"}) 
 
 if __name__ == '__main__':
     app.run()
