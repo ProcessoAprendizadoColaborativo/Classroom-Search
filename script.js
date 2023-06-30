c5e10361d413018b8c768f02977afcdbae9784fb
Eder-R <eder31250@gmail.com>
2023-06-29 21:29:36 -0300

    Ligação API

--- script.js
+++ script.js
@@ -1,45 +1,32 @@
-// Fazer a requisição para a API usando JavaScript
-
-/*fetch('http://localhost:5000/api/dados')  // Substitua a URL pelo endpoint correto da sua API
-  .then(response => response.json())
-  .then(data => {
-    // Manipular os dados recebidos
-    const dadosApiElement = document.getElementById('dados-api');
-    dadosApiElement.innerHTML = JSON.stringify(data);
-  })
-  .catch(error => {
-    console.error('Erro na requisição:', error);
-  });*/
-
-  fetch('http://localhost:5500/salas')
-  .then(response => response.json())
-  .then(data => {
-    // Manipular os dados recebidos
+async function fetchData() {
+  try {
+    const responseSalas = await fetch('http://localhost:3000/salas');
+    const salasData = await responseSalas.json();
+    
     const salasList = document.getElementById('salas-list');
     salasList.innerHTML = '';
-    data.forEach(sala => {
+    salasData.forEach(sala => {
       const salaItem = document.createElement('li');
       salaItem.innerText = `ID: ${sala.id_sala}, Ambiente: ${sala.Ambiente}, Posição: ${sala.Posicao}`;
       salasList.appendChild(salaItem);
     });
-  })
-  .catch(error => {
-    console.error('Erro na requisição:', error);
-  });
 
-fetch('http://localhost:5500/turmas')
-  .then(response => response.json())
-  .then(data => {
-    // Manipular os dados recebidos
+    const responseTurmas = await fetch('http://localhost:3000/turmas');
+    const turmasData = await responseTurmas.json();
+    
     const turmasList = document.getElementById('turmas-list');
     turmasList.innerHTML = '';
-    data.forEach(turma => {
+    turmasData.forEach(turma => {
       const turmaItem = document.createElement('li');
       turmaItem.innerText = `ID: ${turma.id_turma}, Qtd Alunos: ${turma.qtd_alunos}, Turno: ${turma.turno}`;
       turmasList.appendChild(turmaItem);
     });
-  })
-  .catch(error => {
+  } catch (error) {
     console.error('Erro na requisição:', error);
-  });
+  }
+}
 
+// Chame a função fetchData apenas se estiver executando em um ambiente JavaScript real, como um navegador web.
+if (typeof window !== 'undefined') {
+  fetchData();
+}
